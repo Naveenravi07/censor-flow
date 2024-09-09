3dc28303f860caaacf86becd6d3cee9c5707ec1a
Naveenravi07 <gamerkidnav@gmail.com>
2024-09-09 23:18:00 +0530

    Aduio inp

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use cpal::{
+    self,
+    traits::{DeviceTrait, HostTrait, StreamTrait},
+};
+
+fn main() -> Result<()> {
+    let host = cpal::default_host();
+
+    let default_in = host.default_input_device().expect("No default input device available");
+    let default_out = host.default_output_device().expect("No default output device available");
+    println!(" Default in : {:?}", default_in.name()?);
+    println!(" Default out : {:?}", default_out.name()?);
+
+    let mut cfg_in = default_in.supported_input_configs()?;
+    let supported_in = cfg_in.next().expect("No supported input config").with_max_sample_rate();
+    println!("Max supported in : {:?}", supported_in);
+
+    let mut cfg_out = default_out.supported_output_configs()?;
+    let supported_out = cfg_out.next().expect("No supported output config").with_max_sample_rate();
+    println!("Max supported out : {:?}", supported_out);
+
+    let stream = default_in.build_input_stream(
+        &supported_in.config(),
+        move |data: &[f32], _: &cpal::InputCallbackInfo| {
+            println!("Received input data with {} samples", data.len());
+        },
+        on_err,
+        None,
+    )?;
+
+    stream.play()?;
+
+    std::thread::sleep(std::time::Duration::from_secs(5));
+
+    Ok(())
 }
 
+fn on_err(err: cpal::StreamError) {
+    eprintln!("An error occurred in the input stream: {:?}", err);
+}
 
