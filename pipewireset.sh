2332b63e2c4b0639e69774c384134ca4e69f9a40
Naveenravi07 <gamerkidnav@gmail.com>
2024-09-29 16:00:46 +0530

    Minor changes

--- pipewireset.sh
+++
@@ -1,9 +0,0 @@
- #!/bin/sh
-
-systemctl --user restart pipewire-pulse.socket
-
-systemctl --user restart pipewire-pulse.service
-
-systemctl --user restart pipewire.service
-
-systemctl --user restart pipewire.socket 
