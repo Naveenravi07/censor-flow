9d03650d1e4c762877156201c252254bc25c621b
Naveenravi07 <gamerkidnav@gmail.com>
2024-09-30 23:52:27 +0530

    Project port

--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,75 @@
 use anyhow::Result;
-use cpal::{
-    traits::{HostTrait, StreamTrait},
-    HostId,
-};
-use hound::{SampleFormat, WavSpec, WavWriter};
-use rodio::DeviceTrait;
+use hound::{SampleFormat, WavReader, WavSpec, WavWriter};
 use std::{
-    env, fs::File, sync::{mpsc, Arc, Mutex}, thread, time::Duration
+    env,
+    fs::File,
+    process::Command,
+    sync::{mpsc, Arc, Mutex},
 };
 use vosk::{Model, Recognizer};
 
-fn main() -> Result<()> {
-    let mut args = env::args();
-    args.next();
-    let model_path = args.next().expect("Model path not found");
-    let output_path = args.next().expect("Output path not found");
-    let host = cpal::host_from_id(HostId::Alsa).expect("Failed to unwrap host");
+struct AudioConfig {
+    sample_rate: u32,
+    channels: u16,
+    bits_per_sample: u16,
+}
 
-    let default_in = host
-        .default_input_device()
-        .expect("No default input device available");
-    let default_out = host.default_output_device().unwrap();
+impl AudioConfig {
+    fn new(sample_rate: u32, channels: u16, bits_per_sample: u16) -> Self {
+        AudioConfig {
+            sample_rate,
+            channels,
+            bits_per_sample,
+        }
+    }
+}
 
-    let mut cfg_in = default_in.supported_input_configs()?;
-    let supported_in = cfg_in
-        .next()
-        .expect("No supported input config")
-        .with_max_sample_rate();
-
-    let mut cfg_out = default_out.supported_output_configs()?;
-    let supported_out = cfg_out
-        .next()
-        .expect("No supported output config")
-        .with_max_sample_rate();
-
-    let model = Arc::new(Model::new(&model_path).unwrap());
-    let recognizer = Arc::new(Mutex::new(
-        Recognizer::new(&model, supported_in.sample_rate().0 as f32).unwrap(),
-    ));
-    let (tx, rx) = mpsc::channel::<Vec<i16>>();
-
-    // Create a WAV file to save the output audio
-    let spec = WavSpec {
-        sample_format: SampleFormat::Int,
-        channels: supported_out.channels() as u16,
-        sample_rate: supported_out.sample_rate().0 as u32,
-        bits_per_sample: 16,
-    };
-
-    let output_file = File::create(output_path)?;
-    let mut wav_writer = WavWriter::new(output_file, spec)?;
-
-    // Build input stream
-    let reco = Arc::clone(&recognizer);
-    let tx_clone = tx.clone();
-    let input_stream = default_in.build_input_stream(
-        &supported_in.config(),
-        move |data: &[i16], _: &cpal::InputCallbackInfo| {
-            tx_clone.send(data.to_vec()).unwrap();
-            reco.lock().unwrap().accept_waveform(&data);
-            println!("Result {:?}", reco.lock().unwrap().partial_result());
-        },
-        on_err,
-        None,
-    )?;
-    input_stream.play()?;
-
-    //Building output stream
-    let output_stream = default_out.build_output_stream(
-        &supported_out.config(),
-        move |data: &mut [i16], _: &cpal::OutputCallbackInfo| {
-            if let Ok(audio_data) = rx.recv() {
-                for (out_sample, in_sample) in data.iter_mut().zip(audio_data.iter()) {
-                    *out_sample = *in_sample;
-                    wav_writer.write_sample(*in_sample).unwrap();
-                }
-            } else {
-                eprintln!("Receive error ");
-            }
-        },
-        on_err,
-        None,
-    )?;
-    output_stream.play()?;
-
-    thread::sleep(Duration::from_secs(10));
-    //let mut final_result = recognizer.lock().unwrap();
-    //println!("Final Result: {:?}", final_result.result());
+fn extract_audio(video_path: &str, audio_output: &str) -> Result<(), std::io::Error> {
+    Command::new("ffmpeg")
+        .args(&["-i", video_path, "-q:a", "0", "-map", "a", audio_output])
+        .output()?;
     Ok(())
 }
 
-fn on_err(err: cpal::StreamError) {
-    eprintln!("An error occurred in the input/output stream: {:?}", err);
+fn read_wav_samples(file_path: &str) -> Result<(AudioConfig, Vec<i16>)> {
+    let mut reader = WavReader::open(file_path)?;
+    let audio_spec = reader.spec();
+    let samples: Vec<i16> = reader.samples::<i16>().map(|s| s.unwrap()).collect();
+    let conf = AudioConfig::new(
+        audio_spec.sample_rate,
+        audio_spec.channels,
+        audio_spec.bits_per_sample,
+    );
+    Ok((conf, samples))
 }
+
+fn main() -> anyhow::Result<()> {
+    let mut args = env::args();
+    args.next();
+    let model_path = args.next().expect("Model path not found");
+    let video_path = args.next().expect("Video path not found");
+    let output_path = args.next().expect("Output path not found");
+
+    extract_audio(&video_path, "/home/shastri/tmp/censor-flow.wav")?;
+    let (au_cfg, audio_wave) = read_wav_samples("/home/shastri/tmp/censor-flow.wav")?;
+
+    let model = Arc::new(Model::new(&model_path).unwrap());
+    let recognizer = Arc::new(Mutex::new(
+        Recognizer::new(&model, au_cfg.sample_rate as f32).unwrap(),
+    ));
+
+    let spec = WavSpec {
+        sample_format: SampleFormat::Int,
+        channels: au_cfg.channels,
+        sample_rate: au_cfg.sample_rate,
+        bits_per_sample: au_cfg.bits_per_sample,
+    };
+    let output_file = File::create(output_path)?;
+    let mut wav_writer = WavWriter::new(output_file, spec)?;
+
+    let (tx, rx) = mpsc::channel::<Vec<i16>>();
+
+
+    Ok(())
+}
+
