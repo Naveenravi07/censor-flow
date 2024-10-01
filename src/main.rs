10c57a3c1441df66f2774d6a30c33b0a1ed9462a
Naveenravi07 <gamerkidnav@gmail.com>
2024-10-01 20:24:09 +0530

    Audio stream wise processing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use anyhow::Result;
-use hound::{SampleFormat, WavReader, WavSpec, WavWriter};
-use std::{
-    env,
-    fs::File,
-    process::Command,
-    sync::{mpsc, Arc, Mutex},
-};
+use hound::{SampleFormat, WavReader, WavSpec };
+use std::{env, i16, process::Command, sync::Arc, usize};
 use vosk::{Model, Recognizer};
 
+const BUFFER_LEN: usize = 4096;
+
+#[derive(Debug)]
 struct AudioConfig {
     sample_rate: u32,
     channels: u16,
@@ -31,16 +29,35 @@ fn extract_audio(video_path: &str, audio_output: &str) -> Result<(), std::io::Er
     Ok(())
 }
 
-fn read_wav_samples(file_path: &str) -> Result<(AudioConfig, Vec<i16>)> {
-    let mut reader = WavReader::open(file_path)?;
+fn get_audio_config(file_path: &str) -> Result<AudioConfig> {
+    let reader = WavReader::open(file_path)?;
     let audio_spec = reader.spec();
-    let samples: Vec<i16> = reader.samples::<i16>().map(|s| s.unwrap()).collect();
     let conf = AudioConfig::new(
         audio_spec.sample_rate,
         audio_spec.channels,
         audio_spec.bits_per_sample,
     );
-    Ok((conf, samples))
+    Ok(conf)
+}
+
+fn process_audio_in_chunks<F>(file_path: &str, mut cb: F) -> Result<()>
+where
+    F: FnMut(&Vec<i16>) -> Result<()>,
+{
+    let mut buff: Vec<i16> = Vec::with_capacity(BUFFER_LEN);
+    let mut reader = WavReader::open(file_path)?;
+
+    for sample in reader.samples::<i16>() {
+        let sample = sample?;
+        buff.push(sample);
+
+        if buff.len() == BUFFER_LEN {
+            let _ = cb(&buff);
+            buff.clear();
+        }
+    }
+
+    Ok(())
 }
 
 fn main() -> anyhow::Result<()> {
@@ -48,28 +65,33 @@ fn main() -> anyhow::Result<()> {
     args.next();
     let model_path = args.next().expect("Model path not found");
     let video_path = args.next().expect("Video path not found");
-    let output_path = args.next().expect("Output path not found");
+    let output_audio_path = args.next().expect("Output path not found");
 
-    extract_audio(&video_path, "/home/shastri/tmp/censor-flow.wav")?;
-    let (au_cfg, audio_wave) = read_wav_samples("/home/shastri/tmp/censor-flow.wav")?;
+    extract_audio(&video_path, &output_audio_path)?;
+    let au_cfg = get_audio_config(&output_audio_path)?;
+    println!("got audio config : {:?}", au_cfg);
 
     let model = Arc::new(Model::new(&model_path).unwrap());
-    let recognizer = Arc::new(Mutex::new(
-        Recognizer::new(&model, au_cfg.sample_rate as f32).unwrap(),
-    ));
+    let mut recognizer = Recognizer::new(&model, au_cfg.sample_rate as f32).unwrap();
 
-    let spec = WavSpec {
+    let _spec = WavSpec {
         sample_format: SampleFormat::Int,
         channels: au_cfg.channels,
         sample_rate: au_cfg.sample_rate,
         bits_per_sample: au_cfg.bits_per_sample,
     };
-    let output_file = File::create(output_path)?;
-    let mut wav_writer = WavWriter::new(output_file, spec)?;
-
-    let (tx, rx) = mpsc::channel::<Vec<i16>>();
-
+    let _ = process_audio_in_chunks(&output_audio_path, |audio| {
+        let state = recognizer.accept_waveform(audio);
+        match state {
+            vosk::DecodingState::Finalized => {
+                println!("Batch completed ");
+                println!("{:?}", recognizer.final_result());
+            }
+            _ => {}
+        }
+        Ok(())
+    })
+    .unwrap();
 
     Ok(())
 }
-
